dcfcca9ecd8a5f78d9a7f8d708a935ffc4603c0c
Gopi Kapadia <123082735+grk2188@users.noreply.github.com>
2023-07-13 22:06:16 -0400

    Update Love calculator.py

--- Love calculator.py
+++ Love calculator.py
@@ -1,10 +1,8 @@
-# 🚨 Don't change the code below 👇
+
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
 
-# Write your code below this line 👇
 combine = name1 + name2
 combine_lower = combine.lower()
 # print(combine)
